7159b25a5bb3c26f04b57a4dc69c1d57a2efd4ef
kevin nolan <kevin.a.nolan@gmail.com>
2023-04-24 18:27:41 +0100

    Added Burn and DAO and Percentages

--- src/app/services/Models/HolderModel.ts
+++ src/app/services/Models/HolderModel.ts
@@ -3,12 +3,17 @@ import { IChartData } from './IChartData';
 
 export class HolderModel {
   public id!: number;
-  public totalSupply!: number;
   public decimals!: string;
   public holders!: number;
+  public totalSupply!: number;
   public totalInContracts!: number;
   public totalInWallets!: number;
+  public totalInDAO!: number;
   public totalBurned!: number;
+  public totalInContractsPercent!: number;
+  public totalInWalletsPercent!: number;
+  public totalInDAOPercent!: number;
+  public totalBurnedPercent!: number;
   public chartData!: IChartData[];
   public totalBalances!: IBalance[];
 }
