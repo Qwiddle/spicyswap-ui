1b206495ee1619c2efd02bbff045eb4d45833863
Andrew Rubenstein <andjruben@gmail.com>
2023-04-29 21:14:14 -0400

    feat: add design skeleton for pepepot stats & cta

--- src/app/pages/PepePot/index.tsx
+++ src/app/pages/PepePot/index.tsx
@@ -1,3 +1,83 @@
+import { P } from 'app/components/P';
+import pot from './assets/pot.svg';
+import pepeInPot from './assets/pepeinpot.png';
+import styled from 'styled-components';
+import {
+  PotCTA,
+  PotCTAAction,
+  PotCTAButton,
+  PotCTACounter,
+  PotCTAImage,
+  PotCTASize,
+  PotCTASpan,
+  PotDescription,
+  PotHeader,
+  PotStatistics,
+  PotStatisticsItem,
+} from './styles';
+
 export const PepePot = () => {
-  return null;
+  return (
+    <Content>
+      <PotHeader>Pepe Prize Pot</PotHeader>
+      <PotDescription>
+        <span>
+          The pot starts with 75,000 $PEPE. When you click the “Bet” button, you
+          bet 10,000 $PEPE to win the pot. The pot will continue to grow until
+          someone wins and then it starts over.
+        </span>
+      </PotDescription>
+      <PotStatistics>
+        <PotStatisticsItem>
+          <P>Odds to Win Pot</P>
+          <P>5%</P>
+        </PotStatisticsItem>
+        <PotStatisticsItem>
+          <P>Total Wagered</P>
+          <P>1,123,213</P>
+        </PotStatisticsItem>
+        <PotStatisticsItem>
+          <P>PEPE Burned</P>
+          <P>1,123,000</P>
+        </PotStatisticsItem>
+        <PotStatisticsItem>
+          <P>PEPE Sent to DAO</P>
+          <P>1,123,213</P>
+        </PotStatisticsItem>
+      </PotStatistics>
+      <PotCTA>
+        <PotCTAImage>
+          <img
+            src={pepeInPot}
+            alt=""
+            loading="lazy"
+            style={{ position: 'absolute', width: '90px', height: '60px' }}
+          />
+          <PotCTACounter>
+            <PotCTASpan>$PEPE Pot Size</PotCTASpan>
+            <PotCTASize>120,000</PotCTASize>
+            <img loading="lazy" src={pot} alt="" />
+          </PotCTACounter>
+        </PotCTAImage>
+        <PotCTAAction>
+          <span>Placing a bet will wager 10,000 $PEPE.</span>
+          <PotCTAButton>Place your bet 🐸</PotCTAButton>
+          <span>
+            If you lose, 75% of your $PEPE are added to the pot, 12.5% is put
+            into the DAO and 12.5% is burned.
+          </span>
+        </PotCTAAction>
+      </PotCTA>
+    </Content>
+  );
 };
+
+export const Content = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  padding: 20px;
+  gap: 28px;
+  flex-direction: column;
+  align-items: center;
+`;
---
+++ src/app/pages/PepePot/styles.ts
@@ -0,0 +1,136 @@
+import { ConnectButton } from 'app/components/ConnectButton';
+import { HeaderText } from 'app/components/HeaderText';
+import styled from 'styled-components';
+import { media } from 'styles/media';
+
+export const PotHeader = styled(HeaderText)`
+  font-size: 2rem;
+  color: ${p => p.theme.text};
+  font-weight: 600;
+`;
+
+export const PotDescription = styled.div`
+  text-align: center;
+  padding: 0 1rem;
+  color: ${p => p.theme.textSecondary};
+  gap: 1rem;
+  display: flex;
+  flex-direction: column;
+
+  ${media.small} {
+    width: 600px;
+  }
+`;
+
+export const PotStatistics = styled.div`
+  display: flex;
+  gap: 1.25rem;
+  flex-direction: column;
+
+  ${media.small} {
+    flex-direction: row;
+  }
+`;
+
+export const PotStatisticsItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${p => p.theme.primary};
+  padding: 1rem;
+  border-radius: 14px;
+  text-align: center;
+  font-size: 3rem;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 15px 0px;
+
+  > :first-child {
+    color: ${p => p.theme.text};
+    font-size: 1.25rem;
+  }
+`;
+
+export const PotCTA = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  gap: 2rem;
+
+  ${media.small} {
+    flex-direction: row;
+  }
+`;
+
+export const PotCTAImage = styled.div`
+  display: flex;
+  justify-content: center;
+  user-select: none;
+`;
+
+export const PotCTACounter = styled.div`
+  position: relative;
+  height: fit-content;
+  width: fit-content;
+`;
+
+export const PotCTASpan = styled.span`
+  color: white;
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 1.25rem;
+  text-align: center;
+  font-weight: 600;
+  width: 80px;
+`;
+
+export const PotCTASize = styled.span`
+  position: absolute;
+  padding: 12px;
+  top: 70%;
+  left: 50%;
+  border-radius: 6px;
+  transform: translate(-50%, -50%);
+  font-weight: 600;
+  color: ${p => p.theme.textSecondary};
+  background-color: ${p => p.theme.text};
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 15px 0px;
+`;
+
+export const PotCTAAction = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  justify-content: space-around;
+  color: ${p => p.theme.textSecondary};
+  font-size: 0.95rem;
+  padding: 1rem;
+  border-radius: 1rem;
+  max-width: 425px;
+  font-size: 1.25rem;
+  gap: 1.5rem;
+
+  ${media.small} {
+    width: 100%;
+    background-color: initial;
+    height: 275px;
+    gap: 0;
+    padding: 2rem;
+  }
+
+  span:last-of-type {
+    font-size: 12px;
+  }
+`;
+
+export const PotCTAButton = styled(ConnectButton)`
+  height: 40px;
+  color: ${p => p.theme.textSecondary};
+  width: 200px;
+`;
+
+export const PotTableWrapper = styled.div`
+  width: 100%;
+`;
